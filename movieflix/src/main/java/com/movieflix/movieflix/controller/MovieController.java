a715939d6794a638b6b8f850763b62f5c9a14d7c
luanrrsouza <luanrrdsouza@gmail.com>
2025-12-07 15:00:58 -0300

    feat: Adiciona a Entity Movie, com seu controller, repository e service. Feito os relacionamentos e Migrations.

---
+++ movieflix/src/main/java/com/movieflix/movieflix/controller/MovieController.java
@@ -0,0 +1,15 @@
+package com.movieflix.movieflix.controller;
+
+
+import com.movieflix.movieflix.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movieflix/movie")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+}
